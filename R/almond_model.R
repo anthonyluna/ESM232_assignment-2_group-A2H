c6c7d37f124046bba338278f49546889238249cb
anthonyluna <anthonyluna@ucsb.edu>
2021-04-07 15:38:32 -0700

    function is complete with the beginning of the markdown.

---
+++ R/almond_model.R
@@ -0,0 +1,37 @@
+#' 
+
+
+almond_model <- function(clim_data, k1=-0.015 , k2=-0.0046 , k3=-0.07 , k4= 0.0043, intercept = 0.28 ) {
+  ## Error checking
+  ## You can use the library checkmate to
+  ## make this easier, or just do it with ifelse statements
+  
+  # check clim_data dimensions
+  # check clim_data column names
+  # check clim_data column datatypes
+  # check coefficients and intercept value
+  
+  ## End error checking
+  
+  df_summarized <- clim_data %>% 
+    select(year,month,day,tmin_c,precip) %>% 
+    group_by(year,month) %>% 
+    summarize(avg_tmin_c=mean(tmin_c),avg_precip = mean(precip)) %>% 
+    ungroup()
+  
+  df_temp <- df_summarized %>% 
+    filter(month==2) %>% 
+    select(-avg_precip,-month)
+  
+  df_precip <- df_summarized %>% 
+    filter(month==1) %>% 
+    select(-avg_tmin_c,-month)
+  
+  df_out <- full_join(df_temp,df_precip) %>% 
+    mutate(yeild = k1*avg_tmin_c+k2*(avg_tmin_c^2)+k3*avg_precip+k4*(avg_precip^2)+intercept) %>% 
+    select(year,yeild)
+  
+  return(df_out)
+  
+  
+}
