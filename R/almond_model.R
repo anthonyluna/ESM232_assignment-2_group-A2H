054db51283568a8ef524892e47bb70158b1a72c1
Chen Xing <31952927+chen-xxxx@users.noreply.github.com>
2021-04-08 18:24:14 +0800

    Add files via upload
    Fix model calculation error

--- R/almond_model.R
+++ R/almond_model.R
@@ -1,5 +1,15 @@
-#' 
-
+#' Almond Model
+#'
+#' This function computes the yield of almonds
+#’ based on monthly minimum temperature in February and 
+#' accumulated precipitation in January (Lobell 2006)
+#' @param k1 coefficient for Febuary Tmin changes 
+#' @param k2 quadratic term coefficient for Febuary Tmin changes
+#' @param k3 coefficient for January Precipitation changes
+#' @param k4 quadratic term coefficient for January Precipitation changes
+#' @author Anthony Luna, Chen Xing, Atefeh Mohseni
+#' @examples almond_model()
+#' @return Almonds yield anomaly (ton acre-1)
 
 almond_model <- function(clim_data, k1=-0.015 , k2=-0.0046 , k3=-0.07 , k4= 0.0043, intercept = 0.28 ) {
   ## Error checking
@@ -27,9 +37,10 @@ almond_model <- function(clim_data, k1=-0.015 , k2=-0.0046 , k3=-0.07 , k4= 0.00
     filter(month==1) %>% 
     select(-avg_tmin_c,-month)
   
-  df_out <- full_join(df_temp,df_precip) %>% 
-    mutate(yeild = k1*avg_tmin_c+k2*(avg_tmin_c^2)+k3*tot_precip+k4*(tot_precip^2)+intercept) %>% 
-    select(year,yeild)
+  # df_out <- full_join(df_temp,df_precip) %>% 
+  #   mutate(yeild = k1*avg_tmin_c+k2*(avg_tmin_c^2)+k3*tot_precip+k4*(tot_precip^2)+intercept) %>% 
+  #   select(year,yeild)
+  df_out = k1*df_temp$avg_tmin_c+k2*(df_temp$avg_tmin_c^2)+k3*df_precip$tot_precip+k4*(df_precip$tot_precip^2)+intercept
   
   return(df_out)
   
