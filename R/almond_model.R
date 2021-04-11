3af2dcd96aaca595a95bf6c87a11f25539ce7df3
Chen Xing <31952927+chen-xxxx@users.noreply.github.com>
2021-04-11 23:11:57 +0800

    Add annotations and descriptions

--- R/almond_model.R
+++ R/almond_model.R
@@ -17,7 +17,9 @@
 
 almond_model <- function(clim_data, k1=-0.015 , k2=-0.0046 , k3=-0.07 , k4= 0.0043, intercept = 0.28 ) {
   ## Error checking
-
+  ## You can use the library checkmate to
+  ## make this easier, or just do it with ifelse statements
+  
   # check clim_data dimensions
   no_col <- max(count.fields(clim_data, sep = " "))
   if (no_col < 10)
@@ -27,16 +29,16 @@ almond_model <- function(clim_data, k1=-0.015 , k2=-0.0046 , k3=-0.07 , k4= 0.00
     # check clim_data column names
     file_header <- read.table(clim_data, header=TRUE, sep = " ", dec = ".")
     file_headers = colnames(file_header)
-
+    
     expected_header = list("row_number", "D", "day", "month", "year", "wy", "tmax_c", "tmin_c", "precip", "wyd")
-
+    
     check_res <- all.equal(file_headers, expected_headers)
-
+    
     if (check_res) {
       # check clim_data column datatypes
       summary <- summary.default(file_header)
       numeric_types = length(summary[which(summary=="numeric")])
-
+      
       if (numeric_types < 10){
         print("There should be at least 10 numeric data type in the input file!")
       }
@@ -45,16 +47,16 @@ almond_model <- function(clim_data, k1=-0.015 , k2=-0.0046 , k3=-0.07 , k4= 0.00
       print("The format of the input file header is not as expected!")
     }
   }
-
-
+  
+  
   # check coefficients and intercept value
-
+  
   ## End error checking
-
+  
   # Turn the daily minimum temperature into monthly data through monthly average
   # and calculate monthly accumulated precipitation from daily station
   # precipitation data
-
+  
   df_summarized <- clim_data %>% 
     select(year,month,day,tmin_c,precip) %>% 
     group_by(year,month) %>% 
