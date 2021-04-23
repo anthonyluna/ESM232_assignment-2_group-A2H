9f69994a8df363688a2533e7453bf2124a522bdf
atefeh <atefeh@ucsb.edu>
2021-04-22 18:11:55 -0700

    assignment 3 npv script

--- R/npv.R
+++ R/npv.R
@@ -1,19 +1,26 @@
 # Querstions & Notes
 # ----------------------------------------------------------------------------
-# is this the correct discount rate?
-# ----------------------------------------------------------------------------
-npv <- function(yield_outcomes,r = 0.12 , given_yield = 1  , production_cost = 3800  , almond_profit = 2.50  , ton_to_pound = 2000 , start_year = 1989) {
-  # mutate to add column for yield
-  # ------------------------------
-  avg_yield <- yeild_outcomes %>%
-    mutate(est_yield = yeild+given_yield) %>% # anomally -> yield
-    mutate(profit = (est_yield*ton_to_pound*almond_profit)-(production_cost)) %>% # annual profit
-    mutate(time = year-start_year) %>% # add time column
-    mutate(npv = (profit)/(1+r)**time) # calculate npv by year
-  # sum annual npv to get total npv
-  # -------------------------------
-  npv <- dollar(sum(avg_yield$npv))
-  # return desired output
-  # ---------------------
-  return(npv)
-}
+npv <-
+  function(yield_outcomes,
+           r = 0.12 ,
+           given_yield = 1  ,
+           production_cost = 3800  ,
+           almond_profit = 2.50  ,
+           ton_to_pound = 2000 ,
+           start_year = 1989) {
+    # mutate to add column for yield
+    # ------------------------------
+    avg_yield <- yield_outcomes %>%
+      mutate(est_yield = yeild + given_yield) %>% # anomally -> yield
+      mutate(profit = # annual profit
+        (est_yield * ton_to_pound * almond_profit) - (production_cost)
+        ) %>% 
+      mutate(time = year - start_year) %>% # add time column
+      mutate(npv = (profit) / (1 + r) ** time) # calculate npv by year
+    # sum annual npv to get total npv
+    # -------------------------------
+    
+    # return desired output
+    # ---------------------
+    return(avg_yield)
+  }
